abd512577372bcd72c17cdb0f7e9b573b4922542
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2024-01-14 22:37:32 +0500

    Feat: Connection Changes
    - Refactored connection procedure

--- config/db.js
+++ config/db.js
@@ -2,11 +2,17 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DB_URI?.toString(), {
-    useNewUrlParser: true,
-    dbName: 'final_year_project_II', // Specify the database name
-}).then(async() => {
-    console.log("Database Connected Successfully")
-}).catch((error) => {
-    console.log('Error while connecting to database', error)
-})
+const connectDB = async () => {
+    try {
+      const conn = await mongoose.connect(process.env.DB_URI.toString(), {
+        useNewUrlParser: true,
+        dbName: 'final_year_project_II',
+    });
+      console.log(`Database Connected Successfully: ${conn.connection}`);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  }
+
+module.exports = {connectDB}
