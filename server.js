28e5929eb059aafe2f29dfb442bca872682d0da3
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2024-01-14 17:31:25 +0500

    Feat: Added CORS handling

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT;
 
@@ -8,6 +9,7 @@ const { socketInstance } = require("./config/socket")
 const userRouter = require('./router/user-router');
 const courseRouter = require('./router/course-router');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use([userRouter, courseRouter])
