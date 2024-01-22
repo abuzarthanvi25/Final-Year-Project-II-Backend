c75f0d37e7b75139bd4332f2b7a91dab6acb52c8
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2024-01-22 17:55:35 +0500

    bugfix: CORS revert

--- server.js
+++ server.js
@@ -11,13 +11,7 @@ const courseRouter = require('./router/course-router');
 const noteRouter = require('./router/notes-router');
 
 // Enable CORS for all routes
-app.use(cors(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Credentials', true)
-  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use([userRouter, courseRouter, noteRouter]);
