5a2fbd8ddd52e45644ec06942eeb3f4f840ecbf8
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2024-02-03 15:22:26 +0500

    Feat: Environment Changes

--- config/socket.js
+++ config/socket.js
@@ -2,7 +2,7 @@ const { server } = require('../server');
 
 const socketInstance = require("socket.io")(server, {
     cors: {
-      origin: [process.env.FRONTEND_URL,"http://127.0.0.1:5173"],
+      origin: [process.env.FRONTEND_URL1, process.env.FRONTEND_URL2,"http://127.0.0.1:5173"],
       methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     },
 });
